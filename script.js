4381b176afc0cbf06d437659d871c6ec7a5505cc
prasanjit2005 <royprasanjit963@gmail.com>
2025-02-12 12:13:18 +0530

    2nd

--- script.js
+++ script.js
@@ -30,3 +30,24 @@ function typeEffect() {
 }
 
 document.addEventListener("DOMContentLoaded", typeEffect);
+
+// Scroll up animation js code
+document.addEventListener("scroll", function () {
+    let about = document.querySelector(".about");
+    let service = document.querySelector(".services");
+    let scrollPosition = window.scrollY;
+
+    if (scrollPosition > 200) { // Adjust the value as needed
+        about.classList.add("show");
+    }
+    if (scrollPosition > 800) {
+        service.classList.add("show");
+    }
+});
+// left side menu trigger funtion
+document.addEventListener("scroll",function(){
+    let scrollint=window.scrollY;
+    if(scrollint == 100){
+        
+    }
+})
